9ce0fee37ff74ccef78765bc1d6bd96a820dc31b
sunny25984 <72654759+sunny25984@users.noreply.github.com>
2022-02-14 21:19:55 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
 yum install python3-pip
 pip3 install --upgrade pip
-sudo pip3 install numpy
+
